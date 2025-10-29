901c49ac723756116cb735f41c95fee66bda28e5
thanhnguyen <cotut19598@gmail.com>
2025-10-29 22:24:02 +0700

    Add project files.

---
+++ Post.Common/Events/CommentAddedEvent.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Common.Events
+{
+    public class CommentAddedEvent : BaseEvent
+    {
+        public CommentAddedEvent() : base(nameof(CommentAddedEvent))
+        {
+        }
+        public Guid CommentId { get; set; }
+        public String Comment { get; set; }
+        public String Username { get; set; }
+        public DateTime CommentDate { get; set; } 
+    }
+}
