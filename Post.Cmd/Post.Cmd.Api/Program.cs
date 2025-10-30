2b7d84b4cd1870d59f48f27ecf4cd3f578129582
thanhnguyen <cotut19598@gmail.com>
2025-10-30 23:00:12 +0700

    add class

--- Post.Cmd/Post.Cmd.Api/Program.cs
+++ Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,9 +1,18 @@
+using CQRS.Core.Domain;
+using CQRS.Core.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Repositories;
+using Post.Cmd.Infrastructure.Stores;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection(nameof(MongoDBConfig)));
+builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
+builder.Services.AddScoped<IEventStore, EventStore>();
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ Post.Cmd/Post.Cmd.Infrastructure/Stores/EventStore.cs
@@ -0,0 +1,57 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Cmd.Infrastructure.Stores
+{
+    public class EventStore : IEventStore
+    {
+        private readonly IEventStoreRepository _eventRepository;
+        public EventStore(IEventStoreRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<List<BaseEvent>> GetEventAsync(Guid aggreagateId)
+        {
+            var eventStream = await _eventRepository.FindByAggregateId(aggreagateId);
+            if (eventStream == null || !eventStream.Any())
+                throw new AggregateNotFoundException("Incorrect post ID provided");
+            return eventStream.OrderBy(x => x.Version).Select(x=>x.EventData).ToList();
+        }
+
+        public async Task SaveEventAsync(Guid aggregateId, IEnumerable<BaseEvent> events, int expectedVersion)
+        {
+            var eventStream = await _eventRepository.FindByAggregateId(aggregateId);
+            if(expectedVersion != -1 && eventStream[^1].Version != expectedVersion)
+            {
+                throw new ConcurrencyException();
+            }
+            var version = expectedVersion;
+            foreach(var @event in events)
+            {
+                version++;
+                @event.Version = version;
+                var eventType = @event.GetType().Name;
+                var eventModel = new EventModel
+                {
+                    TimeStamp = DateTime.Now,
+                    AggregateIdentifier = aggregateId,
+                    AggregateType = nameof(PostAggregate),
+                    Version = version,
+                    EventType = eventType,
+                    EventData = @event
+                };
+
+                await _eventRepository.SaveAsync(eventModel);
+            }
+        }
+    }
+}
