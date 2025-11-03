eb1b445cfb32c8978c85551ef1baa18f72697193
thanhnguyen <cotut19598@gmail.com>
2025-11-03 22:54:56 +0700

    commit CommandHandler and EventProducer

--- Post.Cmd/Post.Cmd.Api/Program.cs
+++ Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,16 +1,37 @@
+using Confluent.Kafka;
 using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Dispatchers;
+using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.Configure<MongoDBConfig>(builder.Configuration.GetSection(nameof(MongoDBConfig)));
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
-// Add services to the container.
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHander>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+// Register command handler methods
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandleAsync);
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
---
+++ Post.Cmd/Post.Cmd.Infrastructure/Handlers/EventSourcingHander.cs
@@ -0,0 +1,41 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Cmd.Infrastructure.Handlers
+{
+    public class EventSourcingHander : IEventSourcingHandler<PostAggregate>
+    {
+        private readonly IEventStore _eventStore;
+        public EventSourcingHander(IEventStore eventStore)
+        {
+            _eventStore = eventStore;
+        }
+
+        public async Task<PostAggregate> GetByIdAsync(Guid aggregateId)
+        {
+            var aggregate = new PostAggregate();
+            var events = await _eventStore.GetEventAsync(aggregateId);
+            if (events == null && !events.Any())
+            {
+                return aggregate;
+            }
+            aggregate.ReplayEvent(events);
+            aggregate.Version = events.Select(x => x.Version).Max();
+
+            return aggregate;
+        }
+
+        public async Task SaveAsync(AggregateRoot aggregate)
+        {
+            await _eventStore.SaveEventAsync(aggregate.Id, aggregate.GetCommittedChanges(), aggregate.Version);
+            aggregate.MarkChangesAsCommitted();
+        }
+    }
+}
